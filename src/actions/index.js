da92658684c7401f7b0cf71d968811e65da3fdc0
J <socept@gmail.com>
2019-03-28 17:49:27 -0400

    Added the  UserHeader component

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,8 +3,14 @@ import jsonPlaceHolder from '../apis/jsonPlaceHolder';
 // action creator
 export const fetchPosts = () => {
     return async dispatch => {
-        const response = await jsonPlaceHolder.get('/posts');
-        
+        const response = await jsonPlaceHolder.get('/posts');       
         dispatch({ type: 'FETCH_POSTS', payload: response.data });
+    };
+};
+
+export const fetchUser = id => {
+    return async dispatch => {
+        const response = await jsonPlaceHolder.get(`/users/${id}`);
+        dispatch({type: 'FETCH_USER', payload: response.data});
     };
 };
